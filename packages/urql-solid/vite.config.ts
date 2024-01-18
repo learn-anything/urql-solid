8c5052b2dd82f60a06794a44914d4603b5fbf207
Jeet Mandaliya github: sereneinserenade <jeet.mandaliya7@gmail.com>
2024-01-18 23:29:19 +0530

    init: first setup commit
    - set up vite output file types
    - set up deps & more

---
+++ packages/urql-solid/vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, "./src/main.ts"),
+      formats: ["es", "umd"],
+      name: 'urql-solid'
+    },
+  },
+  resolve: { alias: { "~": resolve("src/") } },
+});
